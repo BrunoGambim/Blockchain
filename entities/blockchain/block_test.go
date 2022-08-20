414b266037d0f5d343f560eb9d26bb6db603df70
Bruno Gamim <bruno.gambim@klever.io>
2022-08-20 11:36:58 -0300

    more tests added to block

--- entities/blockchain/block_test.go
+++ entities/blockchain/block_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestDeriveHash(t *testing.T) {
-	prevHash := sha256.Sum256([]byte("teste"))
+	prevHash := sha256.Sum256([]byte("test prev hash"))
 	blockData := "test block"
 
 	hash := sha256.Sum256(bytes.Join([][]byte{[]byte(blockData), prevHash[:]}, []byte{}))
@@ -21,5 +21,32 @@ func TestDeriveHash(t *testing.T) {
 
 	if !reflect.DeepEqual(hash[:], block.Hash) {
 		t.Errorf("Error on hash calc")
+	}
+}
+
+func TestCreateBlock(t *testing.T) {
+	prevHash := sha256.Sum256([]byte("test prev hash"))
+	blockData := "test block"
+
+	block := &Block{
+		Data:     []byte(blockData),
+		PrevHash: prevHash[:],
+	}
+	block.DeriveHash()
+
+	blockCreatedByFunc := CreateBlock(blockData, prevHash[:])
+
+	if !reflect.DeepEqual(blockCreatedByFunc, block) {
+		t.Errorf("Error on block creation")
+	}
+}
+
+func TestGenesis(t *testing.T) {
+	block := CreateBlock("Genesis", []byte{})
+
+	blockCreatedByFunc := Genesis()
+
+	if !reflect.DeepEqual(blockCreatedByFunc, block) {
+		t.Errorf("Error on block genesis")
 	}
 }
